8814af1ced8e26c722efcf619e775b2b1a7fa733
mahnoormahnoorr <mahnoor.mahnoor@csc.fi>
2025-07-23 15:51:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from transcriber import Transcriber
+from summarizer import Summarizer
+import sys
+
+def main(audio_file):
+    print("[1] Transcribing...")
+    transcriber = Transcriber()
+    transcript = transcriber.transcribe(audio_file)
+
+
+
+    print("\n📄 transcript:\n")
+    print(transcript)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <audio_file>")
+    else:
+        main(sys.argv[1])
