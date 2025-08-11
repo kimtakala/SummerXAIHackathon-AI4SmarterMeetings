511d3478aa95e8423d97f19be52e7433f2345e0c
kimtakala <kim.takala@helsinki.fi>
2025-08-11 10:49:16 +0300

    ignore

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from transcriber import Transcriber
-from summarizer import Summarizer
 import sys
+
 
 def main(audio_file):
     print("[1] Transcribing...")
     transcriber = Transcriber()
     transcript = transcriber.transcribe(audio_file)
 
-
-
     print("\n📄 transcript:\n")
     print(transcript)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
